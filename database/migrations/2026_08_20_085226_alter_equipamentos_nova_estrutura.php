5d01c7c0f155fd3d67e45815315303abae63d9fd
Ricardo Fontoura <ricardfo@usp.br>
2026-09-09 11:46:11 -0300

    colocando nullable na constrained

--- database/migrations/2026_08_20_085226_alter_equipamentos_nova_estrutura.php
+++ database/migrations/2026_08_20_085226_alter_equipamentos_nova_estrutura.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('equipamentos', function (Blueprint $table) {
-            $table->foreignId('modelo_switch_id')->after('rack_id')->constrained('modelo_switches');
+            $table->foreignId('modelo_switch_id')->after('rack_id')->nullable()->constrained('modelo_switches');
             $table->integer('ordem')->default(0);
             $table->text('comentario')->nullable()->after('ordem');
 
